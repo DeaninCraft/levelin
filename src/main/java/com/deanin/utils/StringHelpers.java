ac520b48593a3bd1c2a55faef4dac8a3c9177418
Chef <efrisbi1@emich.edu>
2022-04-28 22:38:07 -0400

    Updated getBlockAge to use regex

--- src/main/java/com/deanin/utils/StringHelpers.java
+++ src/main/java/com/deanin/utils/StringHelpers.java
@@ -1,15 +1,23 @@
 package com.deanin.utils;
 
+import com.deanin.levelin.Levelin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringHelpers {
     public static String getBlockName(Block badBlockName) {
         return StringUtils.substringBetween(badBlockName.toString(),"{", "}");
     }
     public static int getBlockStateAge(BlockState blockState) {
-        String age = StringUtils.substringBetween(blockState.toString(),"[", "]");
-        return Integer.parseInt(String.valueOf(age.charAt(age.length()-1)));
+        Pattern ageRegex = Pattern.compile("age=\\d");
+        Matcher matcher = ageRegex.matcher(blockState.toString());
+        if(matcher.find()) {
+            return Integer.parseInt(String.valueOf(matcher.group().charAt(matcher.group().length() - 1)));
+        }
+        return 0;
     }
 }
